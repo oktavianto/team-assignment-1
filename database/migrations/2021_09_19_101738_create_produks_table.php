28dbc74bd0afe40bfbc204f6b13192bcd756576f
Excel Dwi Oktavianto <excelokta@gmail.com>
2021-09-19 17:24:34 +0700

    Initial commit

---
+++ database/migrations/2021_09_19_101738_create_produks_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProduksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produks', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->integer('buy_price');
+            $table->integer('sell_price');
+            $table->text('image');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produks');
+    }
+}
